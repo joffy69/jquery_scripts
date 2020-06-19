0a5a8dc9a65ca7f82d2dc2909806fba83cee0536
joffy69 <jcwgibbs@gmail.com>
2020-06-19 16:53:57 +0000

    back to repl it

--- js/my_script.js
+++ js/my_script.js
@@ -1,4 +1,4 @@
 $(document).ready(funcion(){
-    $('h2').css("text-decoration", "underline"); //underlines all h2
-    $("ul").css("border", "solid 1px #ccc"); //adds a border
+    $('h2').addClass("underline"); //underlines all h2
+    $("ul").addClass("border"); //adds a border
 })
